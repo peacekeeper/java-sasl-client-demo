b3e248687e7d5968e294d74690cb426bebdb5546
Markus Sabadello <markus@danubetech.com>
2025-08-12 02:40:51 +0200

    Add empty integration

---
+++ src/main/java/demo/sasl/client/integration/UserIntegrationNone.java
@@ -0,0 +1,39 @@
+package demo.sasl.client.integration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UserIntegrationNone implements UserIntegration {
+
+    private static final Logger log = LogManager.getLogger(UserIntegrationNone.class);
+
+    @Override
+    public String getName() {
+        log.debug("getName()");
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public String getPassword() {
+        log.debug("getPassword()");
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public String getTextInput() {
+        log.debug("getTextInput()");
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public String getTextInputRealm() {
+        log.debug("getTextInputRealm()");
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public String getTextInputJWK() {
+        log.debug("getTextInputJWK()");
+        throw new UnsupportedOperationException("Not supported.");
+    }
+}
