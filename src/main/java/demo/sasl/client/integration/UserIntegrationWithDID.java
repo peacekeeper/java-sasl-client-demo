79c0b34e1778d11598afc5fd08d39f72d67bcec7
Markus Sabadello <markus@danubetech.com>
2025-12-03 15:11:37 +0100

    Percent-encode the DID

--- src/main/java/demo/sasl/client/integration/UserIntegrationWithDID.java
+++ src/main/java/demo/sasl/client/integration/UserIntegrationWithDID.java
@@ -7,15 +7,9 @@ public class UserIntegrationWithDID implements UserIntegration {
 
     private static final Logger log = LogManager.getLogger(UserIntegrationWithDID.class);
 
-    private static final String DID = "did:key:z6MkfePUhxLV6cM54cgZ4bGmnEdTNm3WDf4arwh5kR3dH51D";
+    private static final String DID = "did%3Akey%3Az6MkfePUhxLV6cM54cgZ4bGmnEdTNm3WDf4arwh5kR3dH51D";
     private static final String PRIVATEKEY = """
-            {
-                "kid": "did:key:z6MkfePUhxLV6cM54cgZ4bGmnEdTNm3WDf4arwh5kR3dH51D#z6MkfePUhxLV6cM54cgZ4bGmnEdTNm3WDf4arwh5kR3dH51D",
-                "kty": "OKP",
-                "crv": "Ed25519",
-                "x": "EbV6-hVmDiD3DKTUgsf2SjjnO7t0ttwMhStQ5JyCFhw",
-                "d": "vGjHIZzZxS3R4mo-V0I_S72ULXDqa2INqkAtuvqJUN8"
-            }
+            {"kid":"did:key:z6MkfePUhxLV6cM54cgZ4bGmnEdTNm3WDf4arwh5kR3dH51D#z6MkfePUhxLV6cM54cgZ4bGmnEdTNm3WDf4arwh5kR3dH51D","kty":"OKP","crv":"Ed25519","x":"EbV6-hVmDiD3DKTUgsf2SjjnO7t0ttwMhStQ5JyCFhw","d":"vGjHIZzZxS3R4mo-V0I_S72ULXDqa2INqkAtuvqJUN8"}
             """;
 
     @Override
